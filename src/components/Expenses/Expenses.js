f1c831c363c712c283e341792006c424908d5360
Eric Lehmann <lehm_eric@yahoo.fr>
2021-08-22 18:34:50 +0200

    add ExpensesFilter + ExpensesList + ExpensesChart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,25 @@
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
+	const [filteredYear, setFilteredYear] = useState("2021");
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear().toString() === filteredYear);
+
 	return (
 		<Card className="expenses">
-			{props.items.map((item) => (
-				<ExpenseItem title={item.title} amount={item.amount} date={item.date} />
-			))}
+			<ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+			<ExpensesChart expenses={filteredExpenses} />
+			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-function NewExpense() {
+function NewExpense(props) {
+	const saveExpenseDataHandler = (enteredExpenseData) => {
+		const expenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString(),
+		};
+		props.onAddExpense(expenseData);
+		setFormIsOpen(false);
+	};
+
+	const [formIsOpen, setFormIsOpen] = useState(false);
+
+	function formIsOpenHandler() {
+		setFormIsOpen(true);
+	}
+
+	function cancelFormHandler() {
+		setFormIsOpen(false);
+	}
 	return (
 		<div className="new-expense">
-			<ExpenseForm />
+			{!formIsOpen && <button onClick={formIsOpenHandler}>Add New Expense</button>}
+			{formIsOpen && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={cancelFormHandler} />}
 		</div>
 	);
 }
